e957cdb1aac18278f58607ff8b14106c5a40fd31
Ayomide Ayodele-Soyebo <midesuperbest@gmail.com>
2023-01-21 05:43:05 +0100

    Bug free

--- models/user.py
+++ models/user.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python3
+"""This is the user class"""
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from models.place import Place
+from models.review import Review
 
-"""This file defines the UserModel class
-It inherits from the BaseModel
-"""
 
-from models.base_model import BaseModel
-
-
-class User(BaseModel):
-    """The User Model"""
-
-    # Attributes
-    email: str = ""
-    password: str = ""
-    first_name: str = ""
-    last_name: str = ""
+class User(BaseModel, Base):
+    """This is the class for user
+    Attributes:
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
+    """
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="user")
+    reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                           backref="user")
